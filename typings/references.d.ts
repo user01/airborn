a354fad16a08dd2dd58f092fad72ed1cc4ec00e0
Erik <erik.langenborg@gmail.com>
2016-08-09 13:35:06 -0400

    Added typings to project

---
+++ typings/references.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="./node.d.ts" />
+/// <reference path="./babylon.2.3.d.ts" />
+/// <reference path="./ramda.d.ts" />
+/// <reference path="./react.d.ts" />
+/// <reference path="./react-dom.d.ts" />
+/// <reference path="./bluebird.d.ts" />
+/// <reference path="./moment.d.ts" />
